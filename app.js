def15cfb0f72c5ad0ffc9cb5870f3b109197cd5a
Stevenson Nelson <sonson521@gmail.com>
2021-07-07 14:00:09 -0400

    append to table

--- app.js
+++ app.js
@@ -8,10 +8,28 @@ function Book(title, author, isbn) {
 
 function UI() {}
 
+UI.prototype.addToList = function (book) {
+  const bookList = document.querySelector('#book-list');
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td>${book.title}<td>
+    <td>${book.author}<td>
+    <td>${book.isbn}<td>
+  `;
+  bookList.appendChild(tr);
+};
+
 form.addEventListener('submit', function (e) {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
+  let book = new Book(title, author, isbn);
+  let ui = new UI(book);
+
+  ui.addToList(book);
+
+  console.log(title, author, isbn);
   e.preventDefault();
 });
